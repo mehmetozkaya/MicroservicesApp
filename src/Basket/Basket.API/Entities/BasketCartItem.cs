bae3ff4f1bf4766b296f7ccf4d8d1847e744f47f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-06-09 22:21:28 +0300

    basket microservices started

---
+++ src/Basket/Basket.API/Entities/BasketCartItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class BasketCartItem
+    {
+        public int Quantity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
