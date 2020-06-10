54c36506b22b6770508fc302ff2213d292fe9b0f
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-06-10 16:55:26 +0300

    basket context added

---
+++ src/Basket/Basket.API/Data/BasketContext.cs
@@ -0,0 +1,22 @@
+﻿using Basket.API.Data.Interfaces;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Data
+{
+    public class BasketContext : IBasketContext
+    {
+        private readonly ConnectionMultiplexer _redisConnection;
+
+        public BasketContext(ConnectionMultiplexer redisConnection)
+        {
+            _redisConnection = redisConnection;
+            Redis = redisConnection.GetDatabase();
+        }
+
+        public IDatabase Redis { get; }
+    }
+}
---
+++ src/Basket/Basket.API/Data/Interfaces/IBasketContext.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Data.Interfaces
+{
+    public interface IBasketContext
+    {
+        IDatabase Redis { get; }
+    }
+}
