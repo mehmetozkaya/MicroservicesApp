ea771458d43b1cdc51f00004d1bf0147790c3e33
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-06-11 16:57:29 +0300

    event bus class lib

---
+++ src/Common/EventBusRabbitMQ/IRabbitMQConnection.cs
@@ -0,0 +1,12 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace EventBusRabbitMQ
+{
+    public interface IRabbitMQConnection : IDisposable
+    {
+        bool IsConnected { get; }
+        bool TryConnect();
+        IModel CreateModel();
+    }
+}
---
+++ src/Common/EventBusRabbitMQ/RabbitMQConnection.cs
@@ -0,0 +1,81 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System;
+using System.Threading;
+
+namespace EventBusRabbitMQ
+{
+    public class RabbitMQConnection : IRabbitMQConnection
+    {
+        private readonly IConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private bool _disposed;
+
+        public RabbitMQConnection(IConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory ?? throw new ArgumentNullException(nameof(connectionFactory));
+            if (!IsConnected)
+            {
+                TryConnect();
+            }
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return _connection != null && _connection.IsOpen && !_disposed;
+            }
+        }
+
+        public bool TryConnect()
+        {
+            try
+            {
+                _connection = _connectionFactory.CreateConnection();
+            }
+            catch (BrokerUnreachableException)
+            {
+                Thread.Sleep(2000);
+                _connection = _connectionFactory.CreateConnection();                
+            }
+
+            if (IsConnected)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public IModel CreateModel()
+        {
+            if (!IsConnected)
+            {
+                throw new InvalidOperationException("No rabbit connection");
+            }
+
+            return _connection.CreateModel();
+        }
+
+        public void Dispose()
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            try
+            {
+                _connection.Dispose();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }        
+    }
+}
