93a62b919e568876d78dbec537b7bb98ee2ac6b0
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-06-12 19:30:27 +0300

    ordering application

---
+++ src/Ordering/Ordering.Application/Mapper/OrderMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Ordering.Application.Commands;
+using Ordering.Application.Responses;
+using Ordering.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Application.Mapper
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            CreateMap<Order, OrderResponse>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+        }
+    }
+}
---
+++ src/Ordering/Ordering.Application/Queries/GetOrderByUserNameQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Ordering.Application.Responses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Application.Queries
+{
+    public class GetOrderByUserNameQuery : IRequest<IEnumerable<OrderResponse>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrderByUserNameQuery(string userName)
+        {
+            UserName = userName ?? throw new ArgumentNullException(nameof(userName));
+        }
+    }
+}
