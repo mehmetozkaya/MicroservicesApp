7771e0f34114014f33958942239bba7aef506c0c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-06-12 18:51:57 +0300

    ordering.infrastructure

---
+++ src/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Entities;
+using Ordering.Core.Repositories;
+using Ordering.Infrastructure.Data;
+using Ordering.Infrastructure.Repositories.Base;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(OrderContext dbContext) : base(dbContext)
+        {
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            var orderList = await _dbContext.Orders
+                                .Where(o => o.UserName == userName)
+                                .ToListAsync();
+            return orderList;
+        }
+    }
+}
