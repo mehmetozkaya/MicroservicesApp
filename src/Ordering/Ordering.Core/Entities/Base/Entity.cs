9c4d53320c908c6d11e7b22762230aa1bdd6c66e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-06-12 15:48:49 +0300

    core layer

---
+++ src/Ordering/Ordering.Core/Entities/Base/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Core.Entities.Base
+{
+    public abstract class Entity : EntityBase<int>
+    {
+    }
+}
---
+++ src/Ordering/Ordering.Core/Entities/Base/IEntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ordering.Core.Entities.Base
+{
+    public interface IEntityBase<TId>
+    {
+        TId Id { get; }
+    }
+}
