421f4a5cd2c5eadb7ae2ab04498d73a57f21c9f8
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2020-06-12 15:28:12 +0300

    ordering microservice

---
+++ src/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+
+    }
+}
